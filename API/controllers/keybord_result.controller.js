497494040c528016f1ad3b88b20e6669f72b9fac
sungmin69355 <sungmin69355@naver.com>
2021-09-01 14:54:51 +0900

    fix: 결과창에 DB값 넘겨 줄수있도록 설계 +(데이터 추가 필요)

--- API/controllers/keybord_result.controller.js
+++ API/controllers/keybord_result.controller.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const ejs = require('ejs');
 const mysql = require('../../config/database'),
       connection = mysql.init();
 mysql.connect(connection);
@@ -5,10 +7,13 @@ mysql.connect(connection);
 const result = {
     post_result : async (req, res, next) =>{
         console.log(req.body);
-        return connection.query('SELECT * FROM keybord', (err, result) =>{
-            res.json(result);
+        fs.readFile('../jota/views/result.ejs', 'utf8', (err, data) => {
+            return connection.query('SELECT * FROM keybord', (err, result) =>{
+                res.send(ejs.render(data, {
+                    data: result
+                  }))
+            });
         })
-        
     }
 }
 module.exports = result;
