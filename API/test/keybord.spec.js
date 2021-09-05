e46dc38428c4affa4ddcf63422ad57f4af0da7cd
sungmin69355 <sungmin69355@naver.com>
2021-09-05 13:26:03 +0900

    fix: 'POST /keybord/result' status 테스트코드 추가

--- API/test/keybord.spec.js
+++ API/test/keybord.spec.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../../app');
 
+//Uint Test
 describe('GET /keybord', () =>{
     it('return 200 status code', (done) =>{
         request(app)
@@ -13,5 +14,41 @@ describe('GET /keybord', () =>{
                 done();
             });
     })
-})
+});
+
+describe('POST /keybord/result', () =>{
+    //아무것도 안보냈을 경우
+    it('return 400 status code', (done) =>{
+        request(app)
+            .post('/keybord/result')
+            .expect(400)
+            .end((err, res) =>{
+                if(err){
+                    throw err;
+                }
+                done();
+            });
+    });
+    //올바르게 값을 다 넣었을 경우
+    it('return 200 status code ', (done) =>{
+        request(app)
+            .post('/keybord/result')
+            .send({
+                identity: 'student',
+                purpose: 'coding',
+                blow: 'little',
+                sound: 'No_noise',
+                design: 'fancy',
+                price: 'ten'
+            })
+            .expect(200)
+            .end((err, res) =>{
+                if(err){
+                    throw err;
+                }
+                done();
+            });
+    });
+});
+
 module.exports = app;
