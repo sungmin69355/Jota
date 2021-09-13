8214be743af03338da7fce4e20920451c24bcf24
sungmin69355 <sungmin69355@naver.com>
2021-09-13 12:54:36 +0900

    fix: POST: keyboard/result 테스트 케이스 추가

--- API/test/keyboard.spec.js
+++ API/test/keyboard.spec.js
@@ -18,7 +18,7 @@ describe('GET /keyboard', () =>{
 
 describe('POST /keyboard/result', () =>{
     //아무것도 안보냈을 경우
-    it('return 400 status code', (done) =>{
+    it('If you dont do anything', (done) =>{
         request(app)
             .post('/keyboard/result')
             .expect(400)
@@ -30,7 +30,7 @@ describe('POST /keyboard/result', () =>{
             });
     });
     //올바르게 값을 다 넣었을 경우
-    it('return 200 status code ', (done) =>{
+    it('If you put all the values in correctly', (done) =>{
         request(app)
             .post('/keyboard/result')
             .send({
@@ -42,6 +42,43 @@ describe('POST /keyboard/result', () =>{
                 price: 'ten'
             })
             .expect(200)
+            .end((err, res) =>{
+                if(err){
+                    throw err;
+                }
+                done();
+            });
+    });
+    //값이 몇 개 없을 경우
+    it('If you dont have it, value', (done) =>{
+        request(app)
+            .post('/keyboard/result')
+            .send({
+                //identity: 'student',
+                purpose: 'coding',
+                //blow: 'little',
+                sound: 'No_noise',
+                //design: 'fancy',
+                //price: 'ten'
+            })
+            .expect(400)
+            .end((err, res) =>{
+                if(err){
+                    throw err;
+                }
+                done();
+            });
+    });    
+    //값을 잘못 입력 했을 경우
+    it('If you input error', (done) =>{
+        request(app)
+            .post('/keyboard/result')
+            .send({
+                test:"sadkasmd",
+                name:"popop1234",
+                error:"4040"
+            })
+            .expect(400)
             .end((err, res) =>{
                 if(err){
                     throw err;
