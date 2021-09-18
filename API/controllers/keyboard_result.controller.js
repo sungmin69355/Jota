4d0849c005f2062475c7c746003e263d916ffcfb
sungmin69355 <sungmin69355@naver.com>
2021-09-18 21:53:30 +0900

    fix: POST /keyboard/result 예외처리 추가

--- API/controllers/keyboard_result.controller.js
+++ API/controllers/keyboard_result.controller.js
@@ -2,18 +2,21 @@ const keyboard_model = require('../models/keyboard.model');
 
 const result = {
     post_result : async (req, res, next) =>{
-        let body = req.body;
-        console.log(body);
-        const KeyboardModel = await keyboard_model.keyboard_model(body);
-        //DB에서 reject을 줬을경우 예외처리를 해줘야함.
-        if(KeyboardModel == undefined){
-            KeyboardModel = [];
+        try{
+            let body = req.body;
+            console.log(body);
+            const KeyboardModel = await keyboard_model.keyboard_model(body);
+            //DB에서 reject을 줬을경우 예외처리를 해줘야함.
+            if(KeyboardModel == undefined){
+                KeyboardModel = [];
+            }
+            console.log("KeyboardModel : ", KeyboardModel);
+            return res.status(200).render("result.ejs", {
+                data : KeyboardModel
+            });
+        }catch(err){
+            return res.status(400).render("error.ejs", {err});
         }
-
-        console.log("KeyboardModel : ", KeyboardModel);
-        return res.status(200).render("result.ejs", {
-            data : KeyboardModel
-        });
     }
 }
 module.exports = result;
